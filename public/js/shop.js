a2d41ec5316c5fa059c9135031bc5759e1a13f4b
FlyLonyx <147259673+FlyLonyx@users.noreply.github.com>
2025-04-01 13:01:35 +0200

    fix - many fix

--- public/js/shop.js
+++ public/js/shop.js
@@ -1,3 +1,6 @@
+const burger = document.querySelector('.burger');
+const navLinks = document.querySelector('.nav-links');
+
 let currentSlide = 0;
 
 function showSlide(slideIndex) {
@@ -9,6 +12,20 @@ function showSlide(slideIndex) {
 
     // Mise à jour de la position
     offers.style.transform = `translateX(-${currentSlide * 100}%)`;
+}
+
+burger.addEventListener('click', toggleMenu);
+
+document.addEventListener('click', (e) => {
+    if (!burger.contains(e.target) && !navLinks.contains(e.target)) {
+        burger.classList.remove('active');
+        navLinks.classList.remove('active');
+    }
+});
+
+function toggleMenu() {
+    burger.classList.toggle('active');
+    navLinks.classList.toggle('active');
 }
 
 function nextSlide() {
